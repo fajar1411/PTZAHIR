6781e6015b2a4cca9887dfd071a0a953d5065d40
Fajar Rizky <56773371+fajar1411@users.noreply.github.com>
2023-04-17 23:46:24 +0700

    Update migrate.go

--- migrasi/migrate.go
+++ migrasi/migrate.go
@@ -1,14 +1,14 @@
 package migrasi
 
 import (
-	activities "todo/fitur/activities/data"
-	todos "todo/fitur/todos/data"
+	_activities "todo/fitur/activities/data"
+	_todos "todo/fitur/todos/data"
 
 	"gorm.io/gorm"
 )
 
 func MigrateDB(db *gorm.DB) {
-	db.AutoMigrate(&activities.Activities{})
-	db.AutoMigrate(&todos.Todo{})
+	db.AutoMigrate(&_activities.Activities{})
+	db.AutoMigrate(&_todos.Todo{})
 
 }
